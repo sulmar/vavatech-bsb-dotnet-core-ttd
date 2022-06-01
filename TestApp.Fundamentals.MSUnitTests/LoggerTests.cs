7f49554c8f938341f3c13c7c56d2e6e4d9f403e2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-06-01 14:11:38 +0200

    Dodanie przykładu testowania zdarzenia

---
+++ TestApp.Fundamentals.MSUnitTests/LoggerTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Fundamentals.MSUnitTests
+{
+    [TestClass]
+    public class LoggerTests
+    {
+        private Logger logger;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            logger = new Logger();
+        }
+
+        // Method_scenario_expectedbehavior
+        [TestMethod]
+        public void Log_EmptyMessage_ShouldThrowsArgumentNullException()
+        {
+            // Arrange
+
+            // Act
+            Action act = () => logger.Log(string.Empty);
+
+            // Assert
+            Assert.ThrowsException<ArgumentNullException>(act);
+        }
+
+        [TestMethod]
+        public void Log_ValidMessage_ShouldSetLastMessage()
+        {
+            // Arrange
+
+            // Act
+            logger.Log("a");
+
+            // Assert
+            Assert.AreEqual("a",logger.LastMessage);
+        }
+
+        [TestMethod]
+        public void Log_ValidMessage_ShouldRaiseMessageLoggedEvent()
+        {
+            // Arrange
+            DateTime logDate = DateTime.MinValue;
+            logger.MessageLogged += (sender, args) => logDate = args;
+
+            // Act
+            logger.Log("a");
+
+            // Assert
+            Assert.AreNotEqual(DateTime.MinValue, logDate);
+        }
+
+        [TestMethod]
+        public void Log_ValidMessage_ShouldRaiseMessageLoggedEvent2()
+        {
+            // Arrange
+            DateTime? logDate = null;
+            logger.MessageLogged += (sender, args) => logDate = args;
+
+            // Act
+            logger.Log("a");
+
+            // Assert
+            Assert.IsTrue(logDate.HasValue);
+        }
+
+        [TestMethod]
+        public void Log_ValidMessage_ShouldRaiseStartEvent()
+        {
+            // Arrange
+            bool isRaised = false;
+            logger.Started += (sender, args) => isRaised = true;
+
+            // Act
+            logger.Log("a");
+
+            // Assert
+            Assert.IsTrue(isRaised);
+        }
+
+
+    }
+}
--- TestApp/Fundamentals/Logger.cs
+++ TestApp/Fundamentals/Logger.cs
@@ -6,21 +6,31 @@ namespace TestApp
 {
     public class Logger
     {
-        public string LastMessage { get; set; }
+        public string LastMessage { get; private set; }
 
         public event EventHandler<DateTime> MessageLogged;
 
+        public event EventHandler Started;
+
         public void Log(string message)
         {
+            Started?.Invoke(this, EventArgs.Empty);
+
             if (string.IsNullOrWhiteSpace(message))
                 throw new ArgumentNullException();
 
-            LastMessage = message;
+            SetLastMessage(message);
 
             // Write the log to a storage
             // ...
 
             MessageLogged?.Invoke(this, DateTime.UtcNow);
+        }
+
+       
+        private void SetLastMessage(string message)
+        {
+            LastMessage = message;
         }
     }
 }
