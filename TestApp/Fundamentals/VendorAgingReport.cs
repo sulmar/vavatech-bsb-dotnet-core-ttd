59d1fb802018373d95a712500e5a733f79200590
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-06-02 09:37:50 +0200

    Dodanie konstruktora

--- TestApp/Fundamentals/VendorAgingReport.cs
+++ TestApp/Fundamentals/VendorAgingReport.cs
@@ -15,8 +15,11 @@ namespace TestApp.Fundamentals
         public string Title { get; private set; }
         public IDictionary<AgingGroup, decimal> Details { get; private set; }
 
-        public VendorAgingReport()
+        public VendorAgingReport(string title)
         {
+            Title = title;
+            CreateDate = DateTime.Now;
+
             Details = new Dictionary<AgingGroup, decimal>
             {
                 { AgingGroup.Timely, 0 },
@@ -26,79 +29,78 @@ namespace TestApp.Fundamentals
                 { AgingGroup.ExpiredFrom180To360Days, 0 },
                 { AgingGroup.ExpiredOver360Days, 0 },
             };
-        }
+        }       
+    }
 
-        public class VendorAgingCalculator
+    public class VendorAgingCalculator
+    {
+        public VendorAgingReport Calculate(params Invoice[] invoices)
         {
-            public VendorAgingReport Calculate(params Invoice[] invoices)
+            VendorAgingReport vendorAgingReport = new VendorAgingReport($"Raport wiekowania płatności na dzień {DateTime.Today:dd.MM.yyyy}");
+            
+            foreach (var invoice in invoices)
             {
-                VendorAgingReport vendorAgingReport = new VendorAgingReport();
-                vendorAgingReport.CreateDate = DateTime.Now;
-                vendorAgingReport.Title = $"Raport wiekowania płatności na dzień {DateTime.Today:dd.MM.yyyy}";
+                var intervalDays = (invoice.DueDate - DateTime.Now).TotalDays;
 
-                foreach (var invoice in invoices)
+                if (intervalDays < 1)
                 {
-                    var intervalDays = (invoice.DueDate - DateTime.Now).TotalDays;
-
-                    if (intervalDays < 1)
-                    {
-                        vendorAgingReport.Details[AgingGroup.Timely] += invoice.TotalAmount;
-                    }
-                    if (intervalDays > 1 && intervalDays < 30)
-                    {
-                        vendorAgingReport.Details[AgingGroup.ExpiredTo30Days] += invoice.TotalAmount;
-                    }
-                    else if (intervalDays >= 30 && intervalDays < 90)
-                    {
-                        vendorAgingReport.Details[AgingGroup.ExpiredFrom30To90Days] += invoice.TotalAmount;
-                    }
-                    else if (intervalDays >= 90 && intervalDays < 180)
-                    {
-                        vendorAgingReport.Details[AgingGroup.ExpiredFrom90To180Days] += invoice.TotalAmount;
-                    }
-                    else
-                    {
-                        vendorAgingReport.Details[AgingGroup.ExpiredOver360Days] -= invoice.TotalAmount;
-                    }
+                    vendorAgingReport.Details[AgingGroup.Timely] += invoice.TotalAmount;
                 }
-
-
-                return vendorAgingReport;
-
+                if (intervalDays > 1 && intervalDays < 30)
+                {
+                    vendorAgingReport.Details[AgingGroup.ExpiredTo30Days] += invoice.TotalAmount;
+                }
+                else if (intervalDays >= 30 && intervalDays < 90)
+                {
+                    vendorAgingReport.Details[AgingGroup.ExpiredFrom30To90Days] += invoice.TotalAmount;
+                }
+                else if (intervalDays >= 90 && intervalDays < 180)
+                {
+                    vendorAgingReport.Details[AgingGroup.ExpiredFrom90To180Days] += invoice.TotalAmount;
+                }
+                else
+                {
+                    vendorAgingReport.Details[AgingGroup.ExpiredOver360Days] -= invoice.TotalAmount;
+                }
             }
 
 
-        }
-
-        public abstract class Base
-        {
+            return vendorAgingReport;
 
         }
 
-        public class Vendor : Base
-        {
-            public string Name { get; set; }
-        }
-
-        public class Invoice : Base
-        {
-            public DateTime DueDate { get; set; }
-            public Vendor Vendor { get; set; }
-            public decimal TotalAmount { get; set; }
-        }
-
-        /// <summary>
-        /// Struktura wiekowa należności
-        /// </summary>
-        public enum AgingGroup
-        {
-            Timely,                     // <1
-            ExpiredTo30Days,            // 1-30
-            ExpiredFrom30To90Days,      // 31-90
-            ExpiredFrom90To180Days,     // 91-180
-            ExpiredFrom180To360Days,    // 181-360
-            ExpiredOver360Days,         // >360
-        }
 
     }
+
+    public abstract class Base
+    {
+
+    }
+
+    public class Vendor : Base
+    {
+        public string Name { get; set; }
+    }
+
+    public class Invoice : Base
+    {
+        public DateTime DueDate { get; set; }
+        public Vendor Vendor { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+
+    /// <summary>
+    /// Struktura wiekowa należności
+    /// </summary>
+    public enum AgingGroup
+    {
+        Timely,                     // <1
+        ExpiredTo30Days,            // 1-30
+        ExpiredFrom30To90Days,      // 31-90
+        ExpiredFrom90To180Days,     // 91-180
+        ExpiredFrom180To360Days,    // 181-360
+        ExpiredOver360Days,         // >360
+    }
+
+
 }
